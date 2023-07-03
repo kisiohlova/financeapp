f94ef089a7d15d08cbfd5f570c368832129b0d66
kisiohlova <kisioglovan@gmail.com>
2023-07-03 15:03:29 +0300

    add main page content

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,13 @@
 class MainController < ApplicationController
   def index
+    operation_types = ['income', 'expense']
+    @messages = {}
+    operation_types.each do |operation_type|
+      total_amount = Operation.where(operation_type: operation_type)
+                              .where(odate: Time.zone.now.beginning_of_month..Time.zone.now.end_of_month)
+                              .sum(:amount)
+      @messages[operation_type] = total_amount > 0 ? "Total #{operation_type} for #{Time.now.strftime("%B")}: #{total_amount}₴" : "You have no #{operation_type.pluralize} in #{Time.now.strftime("%B")}."
+    end
+  @last_ops = Operation.all.order(odate: :asc).pluck(:description, :operation_type, :amount, :odate, :id).last(5)
   end
 end
