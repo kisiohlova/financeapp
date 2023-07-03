b83b02807c820fc9670912a404f9ce0cf85fb61a
kisiohlova <kisioglovan@gmail.com>
2023-07-03 15:12:11 +0300

    modify view

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -6,7 +6,7 @@ class MainController < ApplicationController
       total_amount = Operation.where(operation_type: operation_type)
                               .where(odate: Time.zone.now.beginning_of_month..Time.zone.now.end_of_month)
                               .sum(:amount)
-      @messages[operation_type] = total_amount > 0 ? "Total #{operation_type} for #{Time.now.strftime("%B")}: #{total_amount}₴" : "You have no #{operation_type.pluralize} in #{Time.now.strftime("%B")}."
+      @messages[operation_type] = total_amount > 0 ? "Total #{operation_type} for #{Time.now.strftime("%B")}: #{total_amount} ₴" : "You have no #{operation_type.pluralize} in #{Time.now.strftime("%B")}."
     end
   @last_ops = Operation.all.order(odate: :asc).pluck(:description, :operation_type, :amount, :odate, :id).last(5)
   end
